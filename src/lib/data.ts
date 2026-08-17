5d151d53cd2ed9b1dab99e9068db9a221ba4dae3
garimatejnanis <garimatejnani@gmail.com>
2026-08-17 13:13:16 +0100

    temporal cambio

---
+++ src/lib/data.ts
@@ -0,0 +1,20 @@
+export const contactoData = [
+  {
+    nombre: "LinkedIn",
+    href: "https://es.linkedin.com/in/garima-ts-6b8252248",
+    image: "/linkedin-logo.svg",
+    alt: "LinkedIn",
+  },
+  {
+    nombre: "Email",
+    href: "https://mail.google.com/mail/?view=cm&fs=1&to=garimatejnani@gmail.com",
+    image: "/gmail-logo.svg",
+    alt: "Email",
+  },
+  {
+    nombre: "GitHub",
+    href: "https://github.com/garimatejnanis/",
+    image: "/github-logo.svg",
+    alt: "GitHub",
+  },
+];
